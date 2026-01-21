d0dfa51a63e788600d57ef3782a5f3503752c1ee
Mohammed Yasir Hashmi <yasirhashmi@google.com>
2026-01-21 23:03:35 +0530

    fix: remove non-relevant files

--- gcp-constraints-custom-constraints/modules-restructure/enforcement/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "applied_policies" {
-  description = "List of the full resource names for the applied organization policies."
-  value       = [for p in google_org_policy_policy.enforce_constraints : p.name]
-}
-
-output "enforcement_count" {
-  description = "The number of policies successfully enforced."
-  value       = length(google_org_policy_policy.enforce_constraints)
-}
